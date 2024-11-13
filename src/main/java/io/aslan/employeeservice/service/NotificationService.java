a849fc6392b7a47ae57f4a15f80bf63359bfbb69
kilan <kilandav@yahoo.co.uk>
2024-11-13 21:30:04 +0000

    Send a JSON string as the payload instead to the notification service

--- src/main/java/io/aslan/employeeservice/service/NotificationService.java
+++ src/main/java/io/aslan/employeeservice/service/NotificationService.java
@@ -1,12 +1,14 @@
 package io.aslan.employeeservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.aslan.employeeservice.domain.entity.Employee;
 import io.aslan.employeeservice.domain.message.AllowanceUpdateMessage;
-import io.awspring.cloud.sqs.operations.SendResult;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -15,13 +17,17 @@ import java.math.BigDecimal;
 public class NotificationService {
 
     private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
+
     private final String notificationServiceQueueUrl;
     private final SqsTemplate sqsTemplate;
+    private final ObjectMapper objectMapper;
 
     public NotificationService(@Value("${notification.service.queue.url}") String notificationServiceQueueUrl,
-                               SqsTemplate sqsTemplate) {
+                               SqsTemplate sqsTemplate,
+                               ObjectMapper objectMapper) {
         this.notificationServiceQueueUrl = notificationServiceQueueUrl;
         this.sqsTemplate = sqsTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public void sendAllowanceUpdateMessage(Employee employee,
@@ -35,8 +41,19 @@ public class NotificationService {
                 newMonthlyAllowance,
                 employee.getEmail());
 
-        sqsTemplate.send(notificationServiceQueueUrl, payload);
+        sqsTemplate.send(notificationServiceQueueUrl, MessageBuilder.withPayload(toJson(payload))
+                .setHeader("contentType", "application/json")
+                .build());
+
         log.info("Sent allowance update message={}", payload);
+    }
+
+    private String toJson(Object payload) {
+        try {
+            return objectMapper.writeValueAsString(payload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
